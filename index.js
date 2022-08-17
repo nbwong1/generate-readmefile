24f6bd66e92eeafbc2a040f6cbd79455a23394b2
nbwong <nicholas.b.wong@gmail.com>
2022-08-16 21:58:43 -0700

    the questions and the generate markdown file have been added

--- index.js
+++ index.js
@@ -3,52 +3,93 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [
-  "What is your motivation?, Why did you build this project?,What problems does it solve?, What did you learn?, What makes your project stand out?",
-  "Table of Contents (if necessary)",
-  "What are the steps required to install this project?",
-  "What will it be used for?",
-  "Who would you like to credit for this project?",
-  "What can/can't other developers use this project for?",
-  "List all the features.",
-];
+
+const generateMarkdown = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contributions,
+  tests,
+  questions,
+}) => `# ${title}
+---
+## Description
+${description}
+
+## Table of Contents
+* Installation
+* Usage
+* Licenses
+* Contributions
+* Tests
+* Questions 
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Licenses
+${license}
+
+## Contributions
+${contributions}
+
+## Tests
+${tests}
+
+#Questions
+${questions}`;
 
 inquirer
   .prompt([
     {
       type: "input",
-      name: "description",
-      message: questions[0],
+      name: "title",
+      message: "What is the title of the project?",
     },
     {
       type: "input",
-      name: "tableOfContents",
-      message: questions[1],
+      name: "description",
+      message:
+        'What was your motivation? Why did you build this project? (Note: the answer is not "Because it was a homework assignment.) What problem does it solve? What did you learn? What makes your project stand out?',
     },
     {
       type: "input",
       name: "installation",
-      message: questions[2],
+      message:
+        "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
     },
     {
       type: "input",
       name: "usage",
-      message: questions[3],
-    },
-    {
-      type: "input",
-      name: "credits",
-      message: questions[4],
+      message:
+        "Provide instructions and examples for use.Include screenshots as needed. To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:```md ![alt text](assets/images/screenshot.png) ```",
     },
     {
       type: "input",
       name: "license",
-      message: questions[5],
+      message:
+        "The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/)",
     },
     {
       type: "input",
-      name: "features",
-      message: questions[6],
+      name: "contributions",
+      message:
+        "List your collaborators along with any available GitHub profiles.",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Provide examples on how to run tests here.",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "Any questions along the way?",
     },
   ])
   .then((data) => {
@@ -56,7 +97,7 @@ inquirer
 
     fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
       err ? console.log(err) : console.log("Success!")
-    );    
+    );
   });
 
 // TODO: Create a function to write README file
